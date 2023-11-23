fdab5be6c1651515bbd75fcebdd54496b695ce02
akame1304 <chiaramenghini1304@gmail.com>
2023-11-23 12:54:38 +0100

    finito tranne chen model

--- algorithms/Chen_model.js
+++ algorithms/Chen_model.js
@@ -1,20 +1,47 @@
-function generateChenModelBinaryPath(numSteps, stepSize, initialVolatility, kappa, theta, sigma) {
+function generateChenModelPath(numSteps, stepSize, initialPrice, initialVolatility, kappa, theta, sigma) {
     // Initialize variables
-    let binaryPath = [1]; // Iniziamo con un valore arbitrario, ad esempio 1.
+    let pricePath = [initialPrice];
+    let currentPrice = initialPrice;
 
     // Generate the path
     for (let i = 1; i < numSteps; i++) {
-        // Generate a random value from a normal distribution
+        // Generate random values from normal distributions
+        const epsilonPrice = randn_bm();
         const epsilonVolatility = randn_bm();
 
         // Update volatility using the Chen model
         const dWVolatility = Math.sqrt(stepSize) * epsilonVolatility;
-        const volatilityIncrement = kappa * (theta - binaryPath[i - 1]) * stepSize + sigma * binaryPath[i - 1] * dWVolatility;
-        const newVolatility = Math.sign(binaryPath[i - 1] + volatilityIncrement);
+        const volatilityIncrement = kappa * (theta - initialVolatility) * stepSize + sigma * initialVolatility * dWVolatility;
+        const newVolatility = Math.max(initialVolatility + volatilityIncrement, 0);
 
-        // Add the new value to the path
-        binaryPath.push(newVolatility);
+        // Update stock price using the Chen model
+        const dWPrice = Math.sqrt(stepSize) * epsilonPrice;
+        const diffusion = initialVolatility * Math.sqrt(stepSize) * dWPrice;
+        currentPrice += diffusion;
+
+        // Add the new values to the paths
+        pricePath.push(currentPrice);
     }
 
-    return binaryPath;
+    return pricePath;
 }
+
+// Function to generate a random value from a normal distribution
+function randn_bm() {
+    let u = 0, v = 0;
+    while (u === 0) u = Math.random(); // Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+}
+
+// Example usage
+// const numSteps = 100;          // Number of steps
+// const stepSize = 0.01;         // Step size
+// const initialPrice = 100.0;    // Initial stock price
+// const initialVolatility = 0.2; // Initial volatility
+// const kappa = 1.0;             // Mean reversion speed for volatility
+// const theta = 0.2;             // Long-term mean for volatility
+// const sigma = 0.3;             // Volatility parameter
+
+// const chenModelPath = generateChenModelPath(numSteps, stepSize, initialPrice, initialVolatility, kappa, theta, sigma);
+// console.log("Chen Model Stock Price Path:", chenModelPath);
